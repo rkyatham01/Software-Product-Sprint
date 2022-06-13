bd306c0fbd2037cacdcca4d21eff7e46097aed37
Rish <rkyatham01@gmail.com>
2022-06-13 23:51:42 +0000

    testingifgsonworks

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,19 +1,25 @@
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
+
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Rishith Kyatham!</h1>");
-    response.getWriter().println("<p>This is some content that is being transferred </p>");
-  }
+    response.setContentType("text/html;"); 
+    String[] messages = {"Hey whats sup", "This is a test message", "Keep this in mind"};
+    // response.getWriter().println("<h1>Hello Rishith Kyatham!</h1>");
+    Gson gson = new Gson();
+    String result = gson.toJson(messages);
+    response.getWriter().println(result);
+ }
 }
