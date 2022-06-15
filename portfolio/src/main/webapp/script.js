05940b74ab193970c6dfbd5b7b70803ab5e7c858
Rish <rkyatham01@gmail.com>
2022-06-15 01:01:30 +0000

    update

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -29,7 +29,7 @@ function addRandomGreeting() {
 async function loadingcontent() {
     const responseFromServer = await fetch('/hello');
     const textdisplayed = responseFromServer[Math.floor(Math.random() * responseFromServer.length)];
-    console.log(textdisplayed);
+    console.log(responseFromServer);
     const dateContainer = document.getElementById('elementcontainer');
     elementcontainer.innerText = textdisplayed;
   }
