6a59ce41c8a2069bc2eff870f0559832609a05bb
Rish <rkyatham01@gmail.com>
2022-06-16 22:10:26 +0000

    testing .json

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -28,10 +28,11 @@ function addRandomGreeting() {
 
 async function loadingContentOfUserInput() {
     const serverResponse  = await fetch('/hello');
-    const textdisplayed = await serverResponse .text();
-    let result = (textdisplayed.slice(1, textdisplayed.length-2));
-    let splitresult = result.split(',');
-    const returnedstring = splitresult[Math.floor(Math.random() * splitresult.length)];
+    //const textdisplayed = await serverResponse .text();
+    //let result = (textdisplayed.slice(1, textdisplayed.length-2));
+    //let splitresult = result.split(',');
+    const textdisplayed = await serverResponse.json();
+    const returnedstring = textdisplayed[Math.floor(Math.random() * textdisplayed.length)];
     const inputteduserinfo = document.getElementById('element-container');
     inputteduserinfo.innerText = returnedstring;
   }
